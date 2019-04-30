e222dd62d6bc74af002873bf828ec3896c04ff8c
DominikN <dominik.nowak@husarion.com>
2019-04-30 14:42:28 +0200

    added readme

---
+++ ESP32_cli_eeprom.ino
@@ -0,0 +1,227 @@
+#include <WiFi.h>
+#include <Husarnet.h>
+#include <SimpleCLI.h>
+#include <Preferences.h>
+
+const int CLI_PORT = 8001;
+const int LED_PIN = 16;
+
+HusarnetServer server(CLI_PORT);
+
+Preferences preferences;
+
+SimpleCLI cli;
+
+Command conwifi;
+Command husarnet_join;
+Command setled;
+
+void taskInternetCLI( void * parameter );
+
+void conwifiCallback(cmd* c);
+void husarnet_joinCallback(cmd* c);
+void setledCallback(cmd* c);
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  // Read Wi-Fi and Husarnet credentials from NVS memory
+
+  preferences.begin("my-app", false);
+  String ssid = preferences.getString("ssid");
+  String pass = preferences.getString("pass");
+  String hostname = preferences.getString("hostname");
+  String joincode = preferences.getString("joincode");
+  preferences.end();
+
+
+  // Configure a command line interface (CLI)
+
+  conwifi = cli.addCmd("conwifi", conwifiCallback);
+  conwifi.addArg("ssid");
+  conwifi.addArg("pass");
+
+  husarnet_join = cli.addCmd("husarnet_join", husarnet_joinCallback);
+  husarnet_join.addPosArg("joincode");
+  husarnet_join.addPosArg("hostname");
+
+  setled = cli.addCmd("setled", setledCallback);
+  setled.addPosArg("state");
+
+
+  // Connect to Wi-Fi
+
+  Serial.println("Connecting to Wi-Fi: ");
+  if (ssid != "") {
+    Serial.print("SSID:");
+    Serial.println(ssid);
+    Serial.print("PASS:");
+    Serial.println(pass);
+
+    WiFi.begin(ssid.c_str(), pass.c_str());
+    for (int j = 0; j < 10; j++) {
+      if (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+        if (j == 9) {
+          Serial.printf("\r\nwrong Wi-Fi credentials. Going to commandline:\r\n");
+          Serial.println("ESP32 commandline");
+          Serial.print("# ");
+          return;
+        }
+      } else {
+        Serial.println("done");
+        Serial.print("IP address: ");
+        Serial.println(WiFi.localIP());
+        break;
+      }
+    }
+  } else {
+    Serial.println("no credentials in memory");
+  }
+
+
+  // Connect to Husarnet network
+
+  Serial.println("Connecting to Husarnet: ");
+  if (hostname != "") {
+    Serial.print("joincode:");
+    Serial.println(joincode);
+    Serial.print("hostname:");
+    Serial.println(hostname);
+
+    Husarnet.join(joincode.c_str(), hostname.c_str());
+    Husarnet.start();
+
+    xTaskCreate(
+      taskInternetCLI,  /* Task function. */
+      "taskWifi",       /* String with name of task. */
+      20000,            /* Stack size in bytes. */
+      NULL,             /* Parameter passed as input of the task */
+      2,                /* Priority of the task. */
+      NULL);            /* Task handle. */
+
+  } else {
+    Serial.println("no credentials in memory");
+  }
+
+  Serial.println("ESP32 commandline");
+  Serial.print("# ");
+}
+
+String input;
+
+void loop() {
+
+  // Check if user typed something into the serial monitor
+  if (Serial.available()) {
+    char c = Serial.read();
+    input += c;
+    if (c == '\r') {
+      cli.parse(input);
+      input = "";
+    } else {
+      Serial.print(c);
+    }
+  }
+
+  // Check for parsing errors
+  if (cli.errored()) {
+    // Get error out of queue
+    CommandError cmdError = cli.getError();
+
+    // Print the error
+    Serial.print("ERROR: ");
+    Serial.println(cmdError.toString());
+
+    // Print correct command structure
+    if (cmdError.hasCommand()) {
+      Serial.print("Did you mean \"");
+      Serial.print(cmdError.getCommand().toString());
+      Serial.println("\"?");
+    }
+    Serial.print("\r\n# ");
+  }
+}
+
+
+void conwifiCallback(cmd* c) {
+  Command cmd(c);
+
+  String ssid = cmd.getArg("ssid").getValue();
+  String pass = cmd.getArg("pass").getValue();
+
+  preferences.begin("my-app", false);
+  preferences.putString("ssid", ssid);
+  preferences.putString("pass", pass);
+  preferences.end();
+
+  Serial.println("\r\nWi-Fi credentials saved. Reset to connect.");
+}
+
+void husarnet_joinCallback(cmd* c) {
+  Command cmd(c);
+
+  String hostname = cmd.getArg("hostname").getValue();
+  String joincode = cmd.getArg("joincode").getValue();
+
+  preferences.begin("my-app", false);
+  preferences.putString("hostname", hostname);
+  preferences.putString("joincode", joincode);
+  preferences.end();
+
+  Serial.println("\r\nHusarnet credentials saved. Reset to connect.");
+}
+
+void setledCallback(cmd* c) {
+  Command cmd(c);
+
+  String state = cmd.getArg("state").getValue();
+
+  switch (state.toInt()) {
+    case 0:
+      digitalWrite(LED_PIN, LOW);
+      break;
+    case 1:
+      digitalWrite(LED_PIN, HIGH);
+      break;
+  }
+
+  Serial.println("\r\nHusarnet credentials saved. Reset to connect.");
+}
+
+void taskInternetCLI( void * parameter )
+{
+  server.begin();
+
+  while (1) {
+    HusarnetClient client = server.available();
+    if (client) {
+      Serial.println("New Client.");
+      String currentLine = "";
+      Serial.printf("connected: %d\r\n", client.connected());
+      while (client.connected()) {
+
+        if (client.available()) {
+          char c = client.read();
+          currentLine += c;
+
+          if (c == '\n') {
+            cli.parse(currentLine);
+            currentLine = "";
+            client.println("ok");
+          }
+        }
+      }
+      
+      client.stop();
+      Serial.println("Client disconnected.");
+      Serial.println("");
+    } else {
+      delay(200);
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,84 @@
+# ESP32-http-websocket
+
+**_Communicate with ESP32 over the internet using command line interface to save network credentials in the non-volatile memory and control LED. Written using Arduino framework._**
+
+Command line interface is in many cases the most universal and comfortable way to control things. With CLI you can control your things over the terminal application and in easy way integrate it with your desktop/mobile application.
+
+In this simple project I will demonstrate:
+a) How to use CLI to write network credentials to non-volatile memory. You will be able to change Wi-Fi network credentials without reprogramming your board.
+b) How to connect over the internet to your ESP32 through a Linux terminal
+c) How to control LED over the Internet through CLI
+
+
+In the project we use a few interesing tools:
+
+- **[SimpleCLI library](https://github.com/spacehuhn/SimpleCLI)** - 
+to easily create a command line interface. You don't need to play with parsing strings. That library do that (and much more) for you in a few lines.
+- **[Preferences library](https://github.com/espressif/arduino-esp32/blob/master/libraries/Preferences/src/Preferences.h)** - the most elegant way that I have found to interface to non-volatile/EEPROM/Flash memory during run-time from user code. We use that library to store network credentials in the memory and read them after power-on to connent to the network.
+- **[Husarnet](https://github.com/husarnet/arduino-esp32)** - a Virtual LAN network thanks to which you can access ESP32 both from LAN network and through the internet, without static IP adressess, setting port forwarding on your router etc.
+
+
+To run the project, open Arduino IDE and follow these steps:
+
+**1. Install Husarnet package for ESP32:**
+
+- open `File -> Preferences`
+- in a field **Additional Board Manager URLs** add this link: `https://files.husarion.com/arduino/package_esp32_index.json`
+- open `Tools -> Board: ... -> Boards Manager ...`
+- Search for `esp32-husarnet by Husarion`
+- Click Install button
+
+**2. Select ESP32 dev board:**
+
+- open `Tools -> Board`
+- select **_ESP32 Dev Module_** under "ESP32 Arduino (Husarnet)" section
+
+**3. Install SimpleCLI library:**
+
+- open `Tools -> Manage Libraries...`
+- search for `SimpleCLI`
+- click install button
+
+**4. Program ESP32 board:**
+
+- Open **ESP32_cli_eeprom.ino** project
+- upload project to your ESP32 board.
+
+**5. Get Husarnet join code**
+
+- Register at https://app.husarnet.com/
+- Click **Create network** button, name it (eg. `mynet`), and click **Create** button
+- Click **Add element** button and go to the `join code` tab
+- Copy your join code containging 22 characters `XXXXXXXXXXXXXXXXXXXXXX` and save it for later
+
+**6. Open serial monitor in Arduino IDE:**
+
+- open `Tools -> Serial Monitor`
+- change baudrate to "115200 baud"
+- change line ending to "Both NL & CR"
+
+**7. Save Wi-Fi network credentials in ESP32 non-volatile memory by typing in the Serial Monitor:**
+
+`conwifi -ssid "<YOUR_WIFI_NAME>" -pass "<YOUR_WIFI_PASS>"`
+
+where in `<YOUR_WIFI_NAME>` and `<YOUR_WIFI_PASS>` place your Wi-Fi credentials.
+
+**8. Save Husarnet network credentials in ESP32 non-volatile memory by typing in the Serial Monitor:**
+
+`husarnet_join XXXXXXXXXXXXXXXXXXXXXX myesp32"`
+
+where in `XXXXXXXXXXXXXXXXXXXXXX` is husarnet join code from point 5.
+
+**9. Reset ESP32, on the serial monitor you should see that is trying to connect to Wi-Fi and Husarnet network.
+
+**10. Install Husarnet on your Linux computer and add it to the same network as ESP32**
+
+- open Linux terminal and type this line to install Husarnet: `$ curl https://install.husarnet.com/install.sh | sudo bash`
+- connect your Linux computer to Husarnet network by executing this command: `$ husarnet join XXXXXXXXXXXXXXXXXXXXXX mycomputer"`, where in `XXXXXXXXXXXXXXXXXXXXXX` is husarnet join code from point 5.
+
+**11. Control your ESP32 through CLI over the Internet:
+
+- open Linux terminal  and type this line to connect to ESP32: `$ socat - tcp:myesp32:8001`
+- type `setled 1` to turn on LED connected to pin 16.
+
+Enjoy! :)
